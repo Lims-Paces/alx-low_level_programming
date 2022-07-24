640e3e1898eacb9783376ca649aa8893a4e83b95
Oluwafemi Joy <oluwapamilerin1234@gmail.com>
2022-07-24 05:34:33 -0700

    task 0

---
+++ 0x09-static_libraries/0-isupper.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * _isupper - Check if a letter is upper
+ * @c: The number to be checked
+ *
+ * Return: 1 for upper letter or 0 for any else
+ */
+int _isupper(int c)
+{
+	return (c >= 'A' && c <= 'Z');
+}
